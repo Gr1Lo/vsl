9a235d5beaf9aedadd77c2981373831b67c19a5d
Grigroriy Lozhkin <lozhkin.grig@gmail.com>
2022-07-26 17:16:40 +0300

    Update pca.py

--- pca.py
+++ pca.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 from pyEOF import *
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+import scipy
 
 def visualization(da, pcs, eofs_da, evf, n, dims):
     fig = plt.figure(figsize = (12,2.5*n))
@@ -92,3 +93,77 @@ def pca_tr(trsgi, n = 10, pcas_lower_limit = 0):
     principalComponents = pca.fit_transform(x)
 
     return principalComponents,pca.explained_variance_ratio_
+
+def rot_check(y_pred, y_true, eofs, eigvals, pca, ds_n, ttl, p_type = 'diff', scale_type = 2):
+       
+        '''
+        Визульная оценка работы моделей
+        y_pred - значения главных компонент, 
+        y_true - реальные значения, 
+        eofs - набор значений функций EOF, 
+        eigvals - собственные числа EOF, 
+        pca - объект, полученный в результате eof_an, 
+        ds_n -трехмерный массив, используется для извлечения параметров исходных данных, 
+        ttl - название, 
+        p_type - параметр, на основе которого будут строиться карты
+                  'diff' - модуль разницы
+                  'corr' - коэффициент корреляции Пирсона
+        scale_type - параметр отвечающий за масштабирование главных компонент и 
+                     EOF через умножение/деление значений на собственные числа
+        '''
+
+        if scale_type == 2:
+          eofs = eofs[0:len(eofs)] / np.sqrt(eigvals[0:len(eofs)])[:, np.newaxis]
+          pcs = y_pred[:, 0:len(eofs)] / np.sqrt(eigvals[0:len(eofs)])
+
+        if scale_type == 1:
+          eofs = eofs[0:len(eofs)] * np.sqrt(eigvals[0:len(eofs)])[:, np.newaxis]
+          pcs = y_pred[:, 0:len(eofs)] * np.sqrt(eigvals[0:len(eofs)])
+
+        Yhat = np.dot(pcs, eofs.to_numpy())
+        Yhat = pca._scaler.inverse_transform(Yhat)
+        u = Yhat
+        u0 = y_true
+
+        if p_type=='corr':
+          coor_ar = []
+          for i in range(u0.shape[1]):
+            i0 = u[:,i]
+            i1 = u0[:,i]
+            
+            if ~np.isnan(i0[0]):
+              corr2 = scipy.stats.pearsonr(i0,i1)[0]
+              coor_ar.append(corr2)
+            else:
+              coor_ar.append(np.nan)
+
+          loss0 = np.array(coor_ar)
+          ttl_str = '; среднее значение коэффициента корреляции = '
+          vmin = -1
+          vmax = 1
+
+        else:
+          loss0 = np.abs(u - u0)
+          loss0 = np.where(loss0>50, np.nan, loss0)
+          loss0 = np.mean(loss0,axis=0)
+          ttl_str = '; среднее значение разницы = '
+          vmin = 0
+          vmax = 8
+
+        
+        new = np.reshape(loss0, (-1, ds_n.shape[2]))
+        plt.figure(figsize = (19,10))
+        im = plt.imshow(new[::-1], interpolation='none',
+                        vmin=vmin, vmax=vmax,cmap='jet')
+
+        cbar = plt.colorbar(im,
+                            orientation='vertical')
+        plt.axis('off')
+        plt.tight_layout()
+
+        loss0 = np.nanmean(loss0)
+
+        plt.title(ttl + ttl_str + str(round(loss0,3)),fontsize=20)
+        plt.show()
+
+        return loss0
